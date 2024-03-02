8c2a9b3e7c156176e699b8181e50503d1e1e5753
Obed <obedmontano43@gmail.com>
2024-03-02 14:23:12 -0500

    fixed some errors

--- src/main/java/lc/mine/api/entity/rank/RankInfo.java
+++ src/main/java/lc/mine/api/entity/rank/RankInfo.java
@@ -1,16 +1,12 @@
 package lc.mine.api.entity.rank;
 
 import lombok.Data;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 
 import java.time.Instant;
-import java.util.UUID;
 
 @Data
 public class RankInfo {
-    @Id
-    private UUID uuid;
     @DBRef
     private Rank rank;
     private Boolean permanent;
