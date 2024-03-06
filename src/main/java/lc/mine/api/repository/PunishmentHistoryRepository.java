cebebfde2a079600eb5059578cd7db28ff8cf8bd
Obed <obedmontano43@gmail.com>
2024-03-06 09:07:52 -0500

    fix null punishmentList

--- src/main/java/lc/mine/api/repository/PunishmentHistoryRepository.java
+++ src/main/java/lc/mine/api/repository/PunishmentHistoryRepository.java
@@ -16,7 +16,7 @@ public interface PunishmentHistoryRepository extends MongoRepository<PunishmentH
     @Query("{ 'player' : ?0, 'punishments.active' : true }")
     PunishmentHistory findActivePunishmentsForPlayer(UUID playerId);
 
-    @Query("{ 'punishments.ip' : ?0 }")
+    @Query("{ 'punishmentList.ip' : ?0 }")
     List<PunishmentHistory> findByIp(String ip);
 
     PunishmentHistory findByPlayer(UUID uuid);
